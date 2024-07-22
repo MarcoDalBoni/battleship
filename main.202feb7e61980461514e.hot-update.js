811a3c4d7945d8ce9e1e805d0d36205dd3f57361
Marco Dal Boni <marco.dalboni@expandigroup.com>
2024-07-22 17:07:55 +0200

    fix grafico

---
+++ main.202feb7e61980461514e.hot-update.js
@@ -0,0 +1,30 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatebattleship"]("main",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js!./src/style.css":
+/*!*************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./src/style.css ***!
+  \*************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/noSourceMaps.js */ \"./node_modules/css-loader/dist/runtime/noSourceMaps.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);\n// Imports\n\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, `body {\r\n    display: flex;\r\n    justify-content: center;\r\n    align-items: center;\r\n    gap: 40px;\r\n    height: 95vh;\r\n}\r\n\r\n#sidebar {\r\n    display: flex;\r\n    flex-direction: column;\r\n    align-items: center;\r\n    justify-content: center;\r\n    gap: 20px;\r\n    height: 95vh;\r\n    font-size: 16px;\r\n}\r\n\r\n.wrapper {\r\n    display: flex;\r\n    justify-content: center;\r\n    align-items: center;\r\n    gap: 0px;\r\n}\r\n\r\n#inputName {\r\n    border: 2px solid lightblue;\r\n    font-size: 16px;\r\n    height: 25px;\r\n}\r\n\r\n#submitName,\r\n#resetButton,\r\n#startGame {\r\n    border: none;\r\n    background-color: lightblue;\r\n    font-size: 16px;\r\n    color: white;\r\n    height: 31px;\r\n    cursor: pointer;\r\n}\r\n\r\n#submitName,\r\n#resetButton {\r\n    border-top-right-radius: 8px;\r\n    border-bottom-right-radius: 8px;\r\n    margin: 0px;\r\n}\r\n\r\n#submitName:hover,\r\n#resetButton:hover,\r\n#startGame:hover {\r\n    background-color: rgb(146, 186, 199);\r\n}\r\n\r\n#startGame,\r\n#inputName {\r\n    border-top-left-radius: 8px;\r\n    border-bottom-left-radius: 8px;\r\n}\r\n\r\n.ship-placer,\r\n.toggle-vertical {\r\n    display: flex;\r\n    justify-content: center;\r\n    align-items: center;\r\n}\r\n\r\n.ship-placer {\r\n    gap: 10px;\r\n}\r\n\r\n.toggle-vertical {\r\n    gap: 4px;\r\n}\r\n\r\n.custom-checkbox {\r\n    display: flex;\r\n    justify-content: center;\r\n    align-items: center;\r\n    border: 1px solid lightblue;\r\n    border-radius: 2px;\r\n    height: 20px;\r\n    width: 20px;\r\n    cursor: pointer;\r\n}\r\n\r\n#message {\r\n    font-size: 20px;\r\n    color: red;\r\n}\r\n\r\n#boards {\r\n    display: flex;\r\n    gap: 80px;\r\n    padding: 0px;\r\n}\r\n\r\n.container {\r\n    display: flex;\r\n    width: 430px;\r\n    height: 430px;\r\n    flex-direction: column;\r\n    margin: 0 auto;\r\n}\r\n\r\n.row {\r\n    display: flex;\r\n    flex: 1;\r\n    flex-direction: row;\r\n}\r\n\r\n.cell{\r\n    flex: 1;\r\n    border: 0.2px solid;\r\n    cursor: pointer;\r\n}\r\n\r\n.boat {\r\n    background-color: blue;\r\n    border-color: aquamarine;\r\n    cursor: default;\r\n}\r\n\r\n.miss {\r\n    background-color: gray;\r\n    border-color: rgb(50, 50, 50);\r\n    cursor: default;\r\n}\r\n\r\n.hit {\r\n    background-color: red;\r\n    border-color: rgb(164, 0, 0);\r\n    cursor: default;\r\n}`, \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://battleship/./src/style.css?./node_modules/css-loader/dist/cjs.js");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("cb8c2dcfa76e4563648e")
+/******/ })();
+/******/ 
+/******/ }
+);
